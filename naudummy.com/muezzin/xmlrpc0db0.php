b6644c546afc6200ee75133c9734e519e403cb91
unknown <mudassar70077@gmail.com>
2025-05-19 21:34:40 +0500

    Working only Player and menu

---
+++ naudummy.com/muezzin/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://naudummy.com/muezzin/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://naudummy.com/muezzin/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://naudummy.com/muezzin/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://naudummy.com/muezzin/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://naudummy.com/muezzin/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://naudummy.com/muezzin/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
